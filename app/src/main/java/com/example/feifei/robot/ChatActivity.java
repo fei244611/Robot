7c3ad9a57ed6e828c830112488a769d2944349c5
fei244611 <861835400@qq.com>
2016-09-15 12:18:54 +0800

    添加依赖库

--- app/src/main/java/com/example/feifei/robot/ChatActivity.java
+++ app/src/main/java/com/example/feifei/robot/ChatActivity.java
@@ -1,37 +1,22 @@
 package com.example.feifei.robot;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button btn_chat;
-    private Context context;
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context=this;
-        setContentView(R.layout.activity_main);
-
-        btn_chat= (Button) findViewById(R.id.btn_chat);
-        btn_chat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(context,ChatActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_chat);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
         return true;
     }
 
